d7499a245d99d7db91cb0465804dbf002b2f73b5
Timur Z <engineer@timurz.com>
2019-03-07 17:18:33 +0600

    add selection sort

--- src/sort.js
+++ src/sort.js
@@ -19,6 +19,28 @@ const bubbleSort = (input) => {
   return result
 }
 
+const selectionSort = (input) => {
+  const result = input.slice()
+
+  if (result.length < 2) {
+    return result
+  }
+
+  for (let i = 0; i < result.length - 1; i++) {
+    for (let j = i + 1; j < result.length; j++) {
+      if (result[j] < result[i]) {
+        const temp = result[i]
+
+        result[i] = result[j]
+        result[j] = temp
+      }
+    }
+  }
+
+  return result
+}
+
 module.exports = {
   bubbleSort,
+  selectionSort,
 }
--- src/sort.test.js
+++ src/sort.test.js
@@ -1,9 +1,17 @@
-const { bubbleSort } = require('./sort')
+const { bubbleSort, selectionSort } = require('./sort')
 
 describe('bubbleSort()', () => {
-  it('sorts an array of ints', () => {
+  it('sorts an array of ints in asc order', () => {
     const sample = [8,2,9,4,5,7,3,1,6]
 
     expect(bubbleSort(sample)).toEqual([1,2,3,4,5,6,7,8,9])
+  })
+})
+
+describe('selectionSort()', () => {
+  it('sorts an array of ints in asc order', () => {
+    const sample = [8,2,9,4,5,7,3,1,6]
+
+    expect(selectionSort(sample)).toEqual([1,2,3,4,5,6,7,8,9])
   })
 })
