0f7dbcd4efda86ad88a9ab83a01d8c0eb80ff813
Timur Z <engineer@timurz.com>
2019-05-18 17:20:12 +0600

    clean up

--- src/addends.js
+++ src/addends.js
@@ -10,11 +10,7 @@ const addendsRecursive = (n, sum = 0) => {
 }
 
 const addends = (n) => {
-  let stairs = []
-
-  stairs[0] = 1
-  stairs[1] = 1
-  stairs[2] = 2
+  let stairs = [1,1,2]
 
   for (let i = 3; i <= n; i++) {
       stairs[i] = stairs[i - 1] + stairs[i - 2] + stairs[i - 3]
