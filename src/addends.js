e43bcb8e037579a0f4a8004c6e7ade57bc37d81c
Timur Z <engineer@timurz.com>
2019-05-18 17:02:22 +0600

    add fibonacci way of solving addends

--- src/addends.js
+++ src/addends.js
@@ -1,4 +1,4 @@
-const addends = (n, sum = 0) => {
+const addendsRecursive = (n, sum = 0) => {
   if (sum === n) {
     return 1
   }
@@ -6,9 +6,24 @@ const addends = (n, sum = 0) => {
     return 0
   }
 
-  return addends(n, sum + 1) + addends(n, sum + 2) + addends(n, sum + 3)
+  return addendsRecursive(n, sum + 1) + addendsRecursive(n, sum + 2) + addendsRecursive(n, sum + 3)
+}
+
+const addends = (n) => {
+  let stairs = []
+
+  stairs[0] = 1
+  stairs[1] = 1
+  stairs[2] = 2
+
+  for (let i = 3; i <= n; i++) {
+      stairs[i] = stairs[i - 1] + stairs[i - 2] + stairs[i - 3]
+  }
+
+  return stairs[n]
 }
 
 module.exports = {
+  addendsRecursive,
   addends,
 }
--- src/addends.test.js
+++ src/addends.test.js
@@ -1,4 +1,14 @@
-const { addends } = require('./addends')
+const { addendsRecursive, addends } = require('./addends')
+
+describe('addendsRecursive', () => {
+  it('returns number of possible solutions', () => {
+    expect(addendsRecursive(3)).toEqual(4)
+  })
+
+  it('returns number of possible solutions', () => {
+    expect(addendsRecursive(1)).toEqual(1)
+  })
+})
 
 describe('addends', () => {
   it('returns number of possible solutions', () => {
